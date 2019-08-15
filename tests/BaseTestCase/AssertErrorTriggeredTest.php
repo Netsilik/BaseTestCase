b6ffcf4a220fb91e748cf7e7634d46d61de1ded9
Jacco Vinke <jvinke@motto.nl>
2019-08-15 11:27:50 +0200

    WIP add first test case

--- tests/BaseTestCase/AssertErrorTriggeredTest.php
+++ tests/BaseTestCase/AssertErrorTriggeredTest.php
@@ -7,8 +7,21 @@ namespace Tests\BaseTestCase;
  */
 
 use PHPUnit\Framework\TestCase AS phpUnitTestCase;
+use Tests\Mocks\ConcreteTestCase;
+
 
 class AssertErrorTriggeredTest extends phpUnitTestCase
 {
-
+	public function test_whenFoo_thenBar()
+	{
+		$ctc = new ConcreteTestCase();
+		$ctc->setConvertNoticesToExceptions(false)
+			->setConvertWarningsToExceptions(false)
+			->setConvertErrorsToExceptions(true);
+		
+		
+		$ctc->triggerError('Example warning message', E_USER_WARNING);
+		
+		$result = ConcreteTestCase::assertErrorTriggered(E_USER_WARNING, 'Example warning message');
+	}
 }
